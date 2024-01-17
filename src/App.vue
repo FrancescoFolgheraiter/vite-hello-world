55c2f82e62f00fc433450d969e6536eb5eb49f62
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-01-17 15:07:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import headerApp from './components/headerApp.vue';
+
+export default {
+    data() {
+        return { text:"Vite Hello Wordl"}
+    },
+    components:{
+      headerApp
+    }
+};
+</script>
+
+<template>
+  <div>
+    <headerApp/>
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<style scoped>
+  h1{
+    margin:20px auto;
+    text-align: center;
+  }
+</style>
---
+++ src/components/headerApp.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+    data() {
+        return { text:"Questo è il mio header"}
+    }
+};
+</script>
+
+<template>
+    <div>
+        <header>
+            <h2>{{text}}</h2>
+            <nav>
+              <ul>
+                <li>
+                  <a href="">
+                    Menu 1
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Menu 2
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Menu 3
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Menu 4
+                  </a>
+                </li>
+              </ul>
+            </nav>
+        </header>
+    </div>
+</template>
+
+<style scoped>
+  header{
+    background-color: lightskyblue;
+    border: 1px solid brown;
+    max-width: 1200px;
+    margin:20px auto;
+    padding: 20px;
+    text-align: center;
+  }
+  ul{
+    list-style: none;
+    padding:0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+</style>
